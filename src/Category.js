aa5393ee2040815a69b9215485758753e48db95b
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-29 16:00:46 +0200

    asdasd

--- src/Category.js
+++ src/Category.js
@@ -28,9 +28,7 @@ class Category extends React.Component {
   render() {
     return (
       <div className="category">
-        <h1>Category!</h1>
-        <p>ID: {this.props.params.categoryId}</p>
-        <p>{this.state.category.name}</p>
+        <h1>{this.state.category.name}</h1>
         {this.props.children}
       </div>
     );
