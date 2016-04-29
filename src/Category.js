af70a7d8822e0292a9b07782e6e83ad9a451faac
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-29 12:27:11 +0200

    add link to heroku show page

--- src/Category.js
+++ src/Category.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import jQuery from 'jquery';
+import { Link } from 'react-router';
+
 
 class Category extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      category: null
+    }
+  }
+
+  loadCategory() {
+    let id = this.props.params.categoryId;
+    jQuery.getJSON(`https://peaceful-cove-33360.herokuapp.com/categories/${id}.json`, (function(data){
+      this.setState({
+        category: data.category
+      });
+    }).bind(this));
+  }
+
+  componentDidMount() {
+    this.loadCategory();
+  }
+
   render() {
     return (
       <div className="category">
         <h1>Category!</h1>
         <p>ID: {this.props.params.categoryId}</p>
-
-        {this.props.children}
+        <p>{this.state.category.name}</p>
       </div>
     );
   }
